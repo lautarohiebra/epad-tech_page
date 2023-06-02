139c2410c850603fa593092a478f5ef91f240b00
Lautaro Hiebra <lautarohiebra@gmail.com>
2023-06-02 01:32:28 -0300

    Minimal style changes at Contact page

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -6,8 +6,8 @@ const Contacto = () => {
   return (
     <section className="">
       <FixedBg img="/imgs/contacto.jpg" />
-      <div className='h-[350px] relative'>
-        <h2 className="text-5xl md:text-7xl font-semibold text-base-100 backdrop-grayscale p-5 w-auto absolute bottom-16">Contacto</h2>
+      <div className='h-[350px] flex justify-center align-middle items-center'>
+        <h2 className="text-5xl md:text-7xl font-semibold text-base-100 backdrop-grayscale backdrop-brightness-90 p-5 w-auto h-auto">Contacto</h2>
       </div>
       <div className="bg-white w-full h-[500px] absolute -z-10"></div>
       <div className="sm:px-14">
